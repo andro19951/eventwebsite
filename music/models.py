a7b983eca093c57cd5999cf8c4b0d39310920036
Bidzasheni <george.qutateladze@gmail.com>
2018-02-05 22:38:52 +0200

    elementaruli cvilelebi aba vnaxot erti

--- music/models.py
+++ music/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-
+from CoolApp1 import settings
 
 # Create your models here.
 class Event(models.Model):
     event_title = models.CharField(max_length=250)
-    genre = models.CharField(max_length=100)
+    event_artist = models.CharField(max_length=250)
     event_logo = models.CharField(max_length=1000)
-    time = models.CharField(max_length=1000)
+    time = models.DateField("Event Date:")
     information = models.CharField(max_length=10000)
 
     def __str__(self):
-        return self.event_title + " - " + self.time
+        return self.event_title + " - " + self.event_artist
