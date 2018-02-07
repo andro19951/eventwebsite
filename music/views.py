b6d8d678d70704f6a243a2a8f2355185578574a7
Andro Gvivradze <angviv@ttu.ee>
2018-02-07 19:56:55 +0200

    functional added

--- music/views.py
+++ music/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import  Http404
 from .models import Event
 # Create your views here.
 
+
 def index(request):
     all_events = Event.objects.all()
-    context= dict(all_events=all_events)
+    context = dict(all_events=all_events)
+
     return render(request, 'index.html', context)
 def detail(request, album_id):
+    try:
+        event = Event.objects.get(pk=album_id)
 
-    all_events = Event.objects.all()
+    except Event.DoesNotExist:
+        raise Http404("Event does not exist")
+    return render(request, 'detail.html', dict(event=event))
+
+
+    event_id=str(album_id)
     context= dict(all_events=all_events)
-    return render(request, 'detail.html', context)
+    return render(request, 'detail.html', context,event_id)
     #return HttpResponse("<h2> Details for Album id: "+ str(album_id)+"</h2>")
